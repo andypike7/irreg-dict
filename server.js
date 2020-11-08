2bf3fae841efc04356d864acdcfaaa80272ad282
andypike7 <tech@sym.ru>
2020-11-08 20:04:21 +0300

    MySQL, Express, first handler at Services

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-/* LALALA */
+const express = require('express');
+const config = require('./libs/config');
+const ServiceIrreg1  = require('./services/irreg-1');
+/*
+  COMMANDS via GET requests
+
+  /clear
+    clear and rebuild all databases
+*/
+
+const app = express();
+const expressPort = config.get('express:port');
+
+app.get('/clear', (req, res) => {
+  console.log('*** "/clear" is requested!');
+  res.send({data: `I'm a data`});
+});
+
+app.get('/import/irreg-1', (req, res) => {
+  console.log('*** "/import/irreg-1" is requested!');
+  ServiceIrreg1();
+  res.send({data: `I'm a irreg result!`});
+});
+
+app.listen(expressPort, () => {
+  console.log(`*** Express is listening on port ${expressPort}.`);
+  /*
+  logger.log({
+    level: 'info',
+    message: `*** Express is listening on port ${expressPort}.`,
+    additional: 'properties',
+  });
+  */
+});
---
+++ services/irreg-1.js
@@ -0,0 +1,11 @@
+/*
+  Importing top 10 of irregular verbs from .CSV.
+  Handle data and place result into MySQL.
+*/
+
+const ServiceIrreg1 = () => {
+  console.log('*** I`m at ServiceIrreg1!!!');
+  return 'LALALA!';
+};
+
+module.exports = ServiceIrreg1;
